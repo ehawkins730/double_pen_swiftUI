57c2a5780592e906a9081e00d9a3b931bdb5fe9a
EJ Hawkins <apphawks@gmail.com>
2021-05-24 20:12:16 -0400

    Initial Commit

---
+++ double_pen/double_penApp.swift
@@ -0,0 +1,17 @@
+//
+//  double_penApp.swift
+//  double_pen
+//
+//  Created by Emile Hawkins, Jr on 5/24/21.
+//
+
+import SwiftUI
+
+@main
+struct double_penApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
